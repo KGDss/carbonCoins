17724348403c9e13e6ac52ed0c3920ce449816a7
Nicki Hartmann <nicki.j.hartmann@gmail.com>
2024-03-22 02:13:19 +0700

    adjust z value

--- components/Card.tsx
+++ components/Card.tsx
@@ -23,9 +23,9 @@ const Card: React.FC<CardProps> = ({ image, link }) => {
             exit={{ opacity: 0 }}
           >
             <div className="absolute bg-black pointer-events-none opacity-30 h-full w-full " />
-            <a href={link} target="_blank">
+            <a href={link} target="_blank" className="z-20">
               <motion.h1
-                className="bg-white font-semibold z-10 flex gap-[0.5ch] items-center hover:opacity-75 
+                className="bg-white font-semibold flex gap-[0.5ch] items-center hover:opacity-75 
           text-3xs pl-1 sm:text-2xs md:text-xs md:px-0.5 md:py-0 lg:text-sm lg:px-3 lg:py-2  rounded-full"
                 initial={{ y: 10 }}
                 animate={{ y: 0 }}
@@ -37,7 +37,7 @@ const Card: React.FC<CardProps> = ({ image, link }) => {
                   alt="arrow"
                   height={0}
                   width={0}
-                  className="overflow-hidden w-2 sm:w-4 md:w-6 lg:w-7"
+                  className="w-2 sm:w-4 md:w-6 lg:w-7"
                 ></Image>
               </motion.h1>
             </a>
